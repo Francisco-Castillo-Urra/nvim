bf0cca8cb5803b672a0948e10a65ac38b152efa7
Francisco Castillo Urra <franciscocastillourra@gmail.com>
2022-11-08 13:11:04 -0300

    Actualizado files.lua -Eliminado

---
+++ general.lua
@@ -0,0 +1,55 @@
+-- PDF Reader {{{
+require('adoc_pdf_live').setup {
+    enabled = false,
+    -- The viewer command must be like `command <file>`
+    viewer = 'zathura',
+    binary = 'asciidoctor-pdf',
+    params = '',
+    debug = false,
+    style = '',
+    style_regex = 'style\\.ya?ml'
+}
+-- }}}
+-- Feline {{{
+require("feline").setup({
+})
+--}}}
+-- IndentBlankline {{{
+vim.opt.termguicolors = true
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+
+vim.opt.list = true
+vim.opt.listchars:append("eol:↴")
+
+require("indent_blankline").setup {
+    space_char_blankline = " ",
+    char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+        "IndentBlanklineIndent3",
+        "IndentBlanklineIndent4",
+        "IndentBlanklineIndent5",
+        "IndentBlanklineIndent6",
+    },
+}
+-- }}}
+-- TreeSitter {{{
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+-- }}}
+-- AutoPairs {{{
+require("nvim-autopairs").setup{}
+-- }}}
---
+++ init.vim
@@ -0,0 +1,37 @@
+"Configuraciones generales {{{
+set termguicolors
+set number
+set relativenumber
+set mouse=a
+set foldmethod=marker
+"}}}
+"Plugins {{{
+call plug#begin('$HOME/.config/nvim/plugged')
+Plug 'kyazdani42/nvim-web-devicons' " optional, for file icons
+Plug 'kyazdani42/nvim-tree.lua'
+Plug 'lukas-reineke/indent-blankline.nvim'
+Plug '/goolord/alpha-nvim'
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
+Plug 'feline-nvim/feline.nvim'
+Plug 'catppuccin/nvim', {'as': 'catppuccin'}
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+Plug 'windwp/nvim-autopairs'
+Plug 'marioortizmanero/adoc-pdf-live.nvim'
+Plug 'makerj/vim-pdf'
+Plug 'christoomey/vim-tmux-navigator'
+call plug#end()
+"}}}
+"Atajos de teclado {{{
+nnoremap <C-n> :NvimTreeToggle<CR>
+let mapleader=" "
+
+"}}}
+"Temas{{{
+colorscheme catppuccin
+"}}}
+"Configuraciones Plugins {{{
+so $HOME/.config/nvim/coc.vim
+so $HOME/.config/nvim/files.lua
+so $HOME/.config/nvim/general.lua
+let g:indent_blankline_bufname_exclude = ['.*\.pdf']
+"}}}
