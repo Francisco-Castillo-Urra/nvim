b46b000b50706127384817cbbbd721e541143b61
Francisco Castillo Urra <franciscocastillourra@gmail.com>
2023-03-05 22:47:24 -0300

    Cambiada toda la configuracion de packer por la configuracion bootstrap

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -1,6 +1,19 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
-        use 'wbthomason/packer.nvim'
-        use 'kyazdani42/nvim-web-devicons'
+  use 'wbthomason/packer.nvim'
+          use 'kyazdani42/nvim-web-devicons'
         use 'kyazdani42/nvim-tree.lua'
         use 'lukas-reineke/indent-blankline.nvim'
         use {'neoclide/coc.nvim',branch='release'}
@@ -20,4 +33,7 @@ return require('packer').startup(function(use)
   end,
   requires = {'nvim-tree/nvim-web-devicons'}
 }
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
