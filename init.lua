61eac310c739765d5b30ca2e1c1847e410ff32a8
Francisco Castillo Urra <franciscocastillourra@gmail.com>
2023-03-08 21:49:08 -0300

    shiftwidth 2

--- init.lua
+++ init.lua
@@ -1,13 +1,11 @@
 vim.opt.tabstop = 2
 vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
 vim.opt.termguicolors = true
 vim.opt.relativenumber = true
 vim.opt.mouse = a
 vim.opt.number = true
-
 vim.g.mapleader = " "
-
-vim.api.nvim_set_keymap('','<C-n>','<NvimTreeToggle>', {noremap = true})
 require('plugins/packer')
 require('general')
 require('NvimTree')
